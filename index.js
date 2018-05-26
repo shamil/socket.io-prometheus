bc975fc84c35f8b5fdddc029eed9b24407c89763
Alex Simenduev <shamil.si@gmail.com>
2018-05-26 21:51:36 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,139 @@
+'use strict';
+
+const {Counter, Gauge} = require('prom-client');
+
+let metrics;
+
+function strToBytes(str) {
+  try {
+    str = (typeof str === 'string') ? str : JSON.stringify(str);
+  } catch (e) {
+    return 0;
+  }
+
+  return Buffer.byteLength(str || '', 'utf8');
+}
+
+function beforeHook(obj, methods, hook) {
+  if (!obj) return false;
+  if (!Array.isArray(methods)) methods = [methods];
+
+  methods.forEach((meth) => {
+    let orig = obj[meth];
+    if (!orig) return;
+
+    obj[meth] = function () {
+      try {
+        hook(arguments);
+      } catch (e) {
+        console.error(e);
+      }
+
+      return orig.apply(this, arguments);
+    };
+  });
+}
+
+function initializeMetrics() {
+  return {
+    connectedSockets: new Gauge({
+      name: 'socket_io_connected',
+      help: 'Number of currently connected sockets',
+    }),
+
+    connectTotal: new Counter({
+      name: 'socket_io_connect_total',
+      help: 'Total count of socket.io connection requests',
+    }),
+
+    eventsReceivedTotal: new Counter({
+      name: 'socket_io_events_received_total',
+      help: 'Total count of socket.io recieved events',
+      labelNames: ['event'],
+    }),
+
+    eventsSentTotal: new Counter({
+      name: 'socket_io_events_sent_total',
+      help: 'Total count of socket.io sent events',
+      labelNames: ['event'],
+    }),
+
+    bytesReceived: new Counter({
+      name: 'socket_io_recieve_bytes',
+      help: 'Total socket.io bytes recieved',
+      labelNames: ['event'],
+    }),
+
+    bytesTransmitted: new Counter({
+      name: 'socket_io_transmit_bytes',
+      help: 'Total socket.io bytes transmitted',
+      labelNames: ['event'],
+    }),
+  };
+}
+
+function collectMetrics(io) {
+  if (metrics == null) {
+    metrics = initializeMetrics();
+  }
+
+  const connectedSockets = metrics.connectedSockets;
+  const connectTotal = metrics.connectTotal;
+  const eventsReceivedTotal = metrics.eventsReceivedTotal;
+  const eventsSentTotal = metrics.eventsSentTotal;
+  const bytesReceived = metrics.bytesReceived;
+  const bytesTransmitted = metrics.bytesTransmitted;
+
+  // listen to connect events
+  io.on('connection', (socket) => {
+    connectTotal.inc();
+    connectedSockets.inc();
+    socket.on('disconnect', () => connectedSockets.dec());
+
+    // Sent events
+    beforeHook(socket, 'emit', ([event, eventStr]) => {
+      // ignore internal events
+      if (event === 'newListener') return;
+
+      bytesTransmitted.labels(event).inc(strToBytes(eventStr));
+      eventsSentTotal.labels(event).inc();
+    });
+
+    // Recieved events
+    beforeHook(socket, ['addListener', 'on'], (args) => {
+      let event = args[0],
+        cbPos = args.length - 1;
+
+      // ignore internal events
+      if (event === 'disconnect') return;
+
+      // get original callback function
+      let origCb = (typeof args[cbPos] === 'function') ? args[cbPos] : undefined;
+      if (!origCb) return false;
+
+      args[cbPos] = function () {
+        let eventStr = Array.prototype.slice.call(arguments)[0];
+
+        bytesReceived.labels(event).inc(strToBytes(eventStr));
+        eventsReceivedTotal.labels(event).inc();
+
+        return origCb.apply(this, arguments);
+      };
+    });
+  });
+}
+
+module.exports = function() {
+  try {
+      require.resolve('prom-client');
+  } catch (e) {
+      if (e.code === 'MODULE_NOT_FOUND') {
+          console.log('`prom-client` not available, socket.io metrics will not be collected.');
+          return;
+      }
+
+      throw e;
+  }
+
+  return collectMetrics.apply(null, Array.from(arguments));
+};
---
+++ test.js
@@ -0,0 +1,150 @@
+process.env.NODE_ENV = 'test';
+
+const {EventEmitter} = require('events');
+const {expect} = require('chai');
+const sinon = require('sinon');
+
+const promCollector = require('rewire')('./');
+
+describe('socket.io metrics: internal functions', () => {
+  // retrieve unexported functions
+  let strToBytes = promCollector.__get__('strToBytes'),
+    beforeHook = promCollector.__get__('beforeHook');
+
+  it('strToBytes works with UTF8 string', () => {
+    expect(strToBytes("Шамиль")).to.eq(12);
+  });
+
+  it('strToBytes works with non-UTF8 string', () => {
+    expect(strToBytes("Shamil")).to.eq(6);
+  });
+
+  it('strToBytes works with object', () => {
+    expect(strToBytes({mesage: "Hello World!"})).to.eq(25);
+  });
+
+  it('strToBytes returns 0 on exception or undefined', () => {
+    let obj = {};
+    obj.a = {b: obj};
+
+    expect(strToBytes(obj)).to.eq(0);
+    expect(strToBytes(undefined)).to.eq(0);
+  });
+
+  it('beforeHook returns false if first arg is undefined', () => {
+    expect(beforeHook()).to.eq(false);
+  });
+
+  it('beforeHook must not change returned value', () => {
+    let obj = {func: (a) => a};
+
+    beforeHook(obj, 'func', (a) => {
+      a = 0;
+    });
+    expect(obj.func(10)).to.not.eq(0);
+  });
+
+  it('beforeHook must catch hook exceptions', () => {
+    let obj = {func: (a) => a};
+
+    beforeHook(obj, 'func', () => {
+      throw new Error('something went wrong');
+    });
+    expect(obj.func(10)).to.eq(10);
+  });
+});
+
+describe('socket.io metrics: collector', () => {
+  const socket = new EventEmitter();
+  const io = new EventEmitter();
+
+  promCollector(io);
+
+  // retrieve unexported metrix variable
+  const metrics = promCollector.__get__('metrics');
+
+  const connectedSockets = metrics.connectedSockets;
+  const connectTotal = metrics.connectTotal;
+  const eventsReceivedTotal = metrics.eventsReceivedTotal;
+  const eventsSentTotal = metrics.eventsSentTotal;
+  const bytesReceived = metrics.bytesReceived;
+  const bytesTransmitted = metrics.bytesTransmitted;
+
+  const sandbox = sinon.createSandbox();
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it('on connection - connectTotal and connectedSockets should call increment', () => {
+    sandbox.spy(connectTotal, 'inc');
+    sandbox.spy(connectedSockets, 'inc');
+
+    io.emit('connection', socket);
+    expect(connectTotal.inc.callCount).to.eq(1);
+    expect(connectedSockets.inc.callCount).to.eq(1);
+  });
+
+  it('on socket disconnect - connectedSockets should call decrement', () => {
+    sandbox.spy(connectedSockets, 'dec');
+
+    socket.emit('disconnect');
+    expect(connectedSockets.dec.callCount).to.eq(1);
+  });
+
+  it('on socket emit - bytesTransmitted and eventsSentTotal should increment', () => {
+    let bytesTransmittedInc = sandbox.spy(),
+      eventsSentTotalInc = sandbox.spy();
+
+    sandbox.stub(bytesTransmitted, 'labels').returns({inc: bytesTransmittedInc});
+    sandbox.stub(eventsSentTotal, 'labels').returns({inc: eventsSentTotalInc});
+
+    socket.emit('test event');
+    expect(bytesTransmittedInc.callCount).to.eq(1);
+    expect(eventsSentTotalInc.callCount).to.eq(1);
+  });
+
+  it('on socket emit newListener - bytesTransmitted and eventsSentTotal should not increment', () => {
+    let bytesTransmittedInc = sandbox.spy(),
+      eventsSentTotalInc = sandbox.spy();
+
+    sandbox.stub(bytesTransmitted, 'labels').returns({inc: bytesTransmittedInc});
+    sandbox.stub(eventsSentTotal, 'labels').returns({inc: eventsSentTotalInc});
+
+    socket.emit('newListener');
+    expect(bytesTransmittedInc.callCount).to.eq(0);
+    expect(eventsSentTotalInc.callCount).to.eq(0);
+  });
+
+  it('on socket event - bytesReceived and eventsReceivedTotal should increment', (done) => {
+    let bytesReceivedInc = sandbox.spy(),
+      eventsReceivedTotalInc = sandbox.spy();
+
+    sandbox.stub(bytesReceived, 'labels').returns({inc: bytesReceivedInc});
+    sandbox.stub(eventsReceivedTotal, 'labels').returns({inc: eventsReceivedTotalInc});
+
+    socket.on('event', () => {
+      expect(bytesReceivedInc.callCount).to.eq(1);
+      expect(eventsReceivedTotalInc.callCount).to.eq(1);
+      done();
+    });
+
+    socket.emit('event');
+  });
+
+  it('on socket disconnect event - bytesReceived and eventsReceivedTotal should not increment', (done) => {
+    let bytesReceivedInc = sandbox.spy(),
+      eventsReceivedTotalInc = sandbox.spy();
+
+    sandbox.stub(bytesReceived, 'labels').returns({inc: bytesReceivedInc});
+    sandbox.stub(eventsReceivedTotal, 'labels').returns({inc: eventsReceivedTotalInc});
+
+    socket.on('disconnect', () => {
+      expect(bytesReceivedInc.callCount).to.eq(0);
+      expect(eventsReceivedTotalInc.callCount).to.eq(0);
+      done();
+    });
+
+    socket.emit('disconnect');
+  });
+});
